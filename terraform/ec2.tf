5727a4fff74babd968a1bfa7da787792a1492c83
jayanti dani <jayantidani21@gmil.com>
2026-01-05 17:57:08 +0530

    Add VPC, security group and update EC2 configuration

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,9 +1,22 @@
 resource "aws_instance" "app_server" {
-  ami           = var.ami_id
-  instance_type = "t2.micro"
-  key_name      = var.key_name
+  ami                    = var.ami_id
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
 
+  associate_public_ip_address = true
+  
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    amazon-linux-extras install docker -y
+    systemctl start docker
+    systemctl enable docker
+    usermod -aG docker ec2-user
+  EOF
+  
   tags = {
-    Name = "devops-app-servercd"
+    Name = "devops-app-server"
   }
 }
---
+++ terraform/security-group.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "app_sg" {
+  name        = "devops-app-sg"
+  description = "Allow SSH and App traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "devops-app-sg"
+  }
+}
